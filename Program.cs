6a14585560be7bf269364724ad536126908e740d
Caroline O'Keefe <carolineokeefe@Carolines-MacBook-Pro-2.local>
2021-09-03 09:09:42 -0500

    updated welcome message

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace example_repo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Git is great");
         }
     }
 }
